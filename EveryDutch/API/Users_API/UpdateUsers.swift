5ee2a58c0bde50a286a8eb2d18840db6a2b5aed6
chwl0571@daum.net <chwl0571@daum.net>
2024-06-12 21:30:41 +0900

    make UserProfileVC

--- EveryDutch/API/Users_API/UpdateUsers.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UpdateUsers.swift
-//  EveryDutch
-//
-//  Created by 계은성 on 1/9/24.
-//
-
-import Foundation
-
--- EveryDutch/Utils/Enum/TableCell/ReceiptWriteEnum.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ReceiptWriteEnum.swift
-//  EveryDutch
-//
-//  Created by 계은성 on 2/8/24.
-//
-
-import Foundation
-
